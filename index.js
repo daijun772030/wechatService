512074661ddd9d36f4bdeebabe9179ec26bfbdcf
代鹏 <daipeng@daipengdeMacBook-Pro.local>
2018-09-25 15:21:04 +0800

    add: 分支问题

--- index.js
+++ index.js
@@ -27,8 +27,22 @@ app.use('/wechat', wechat(config, function(req, res, next) {
     // 确定点击的自定义菜单是客服服务按钮，然后就自动回复响应的内容，否则就转发到多客服系统
     if (req.weixin.Event === 'CLICK' && req.weixin.EventKey === 'CUSTOMER_SERVICE') {
         res.reply(`您好！欢迎联系懒猪到家在线技术支持，请选择您需要咨询的内容，并回复对应的数字序号：\n[1] 订单问题 \n[2] 商家问题 \n[3] 投诉建议 \n[4] 商务合作 \n例如：咨询订单问题的相关内容，请回复 1，将由在线客服为您咨询`);
-    }else {
-        res.transfer2CustomerService();
+    } else {
+        switch(req.weixiin.Content) {
+            case '1': 
+                res.reply('订单问题');
+                break;
+            case '2': 
+                res.reply('商家问题');
+                break;
+            case '3': 
+                res.reply('投诉建议');
+                break;
+            case '4': 
+                res.reply('商务合作');
+                break;
+            default: res.transfer2CustomerService();
+        }
     }
 }));
 
